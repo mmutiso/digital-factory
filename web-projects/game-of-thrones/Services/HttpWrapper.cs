676afbec61aef2b1fb8597faec1cec22cbf86a8e
Matthew <mutee88@gmail.com>
2021-02-04 23:41:39 +0300

    Add VS solution and game of thrones project

---
+++ web-projects/game-of-thrones/Services/HttpWrapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using GameOfThrones.Models;
+using System.IO;
+
+namespace GameOfThrones.Services
+{
+    public class HttpWrapper
+    {
+        HttpClient httpClient;
+        AppRuntimeSettings runtimeSettings;
+
+        public HttpWrapper(HttpClient httpClient)
+        {
+            this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        Uri GetPath(string fragment)
+        {
+            var baseUri = new Uri(runtimeSettings.ApiBaseUrl);
+            var endpoint = new Uri(baseUri, fragment);
+
+            return endpoint;
+        }
+
+        public async  Task<T> Get<T>(string relativePath)
+        {
+            Uri uri = GetPath(relativePath);
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<T>(responseBody);
+        }
+    }
+}
---
+++ web-projects/game-of-thrones/Services/RelativePaths.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameOfThrones.Services
+{
+    public static class RelativePaths 
+    {
+        public static string BooksEndpoint => "/api/books";
+        public static string CharactersEndpoint => "/api/characters";
+        public static string HousesEndpoint => "/api/houses";
+    }
+}
